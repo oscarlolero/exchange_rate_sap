0a59cc4eb27cfedcdc639d987c3f40a77104c1a4
Oscar Montes <oscmcojc1@live.com.mx>
2021-05-27 10:39:04 -0500

    Identation

--- index.js
+++ index.js
@@ -12,10 +12,9 @@ const BANXICOToken = 'TOKENBANXICO';
 init().then();
 
 
-
 async function init() {
   const result = await updateSapRates();
-  if(result.success) {
+  if (result.success) {
     const sentMailResult = sendMail(result);
   }
 }
@@ -36,8 +35,8 @@ async function sendMail(data) {
     html: `<h3>Se actualizó correctamente USD y EUR</h3><p>USD: ${data.usd}, EUR: ${data.eur}</p>`
   };
 
-  mailTransporter.sendMail(mailDetails, function(err, data) {
-    if(err) {
+  mailTransporter.sendMail(mailDetails, function (err, data) {
+    if (err) {
       console.log('Problema al enviar el email', err);
     } else {
       console.log('Email enviado correctamente.');
@@ -57,82 +56,82 @@ async function updateSapRates() {
     });
 
     response.data.bmx.series.forEach((element) => {
-      if(element.idSerie === 'SF46410') {
+      if (element.idSerie === 'SF46410') {
         eurValue = element.datos[0].dato;
-      } else if(element.idSerie === 'SF43718') {
+      } else if (element.idSerie === 'SF43718') {
         usdValue = element.datos[0].dato;
       }
     });
     console.log(`VALORES BANXICO: USD: ${usdValue}, EUR: ${eurValue}`);
 
     //SAP
-      const SAPLoginResponse = await axios.post(`https://${serviceLayerURL}/b1s/v1/Login`, {
-        "CompanyDB": "SBODemoMX",
-        "UserName": "manager",
-        "Password": "oscar0001",
-      });
-      const cookie = SAPLoginResponse.headers['set-cookie'];
-      //Set currency USD
-      let SAPResponse = await axios.post(`https://${serviceLayerURL}/b1s/v1/SBOBobService_SetCurrencyRate`, {
-          "Currency": "usd",
-          "RateDate": sapDate,
-          "Rate": usdValue
-        },
-        {
-          headers: {
-            "Cookie": cookie
-          }
+    const SAPLoginResponse = await axios.post(`https://${serviceLayerURL}/b1s/v1/Login`, {
+      "CompanyDB": "SBODemoMX",
+      "UserName": "manager",
+      "Password": "oscar0001",
+    });
+    const cookie = SAPLoginResponse.headers['set-cookie'];
+    //Set currency USD
+    let SAPResponse = await axios.post(`https://${serviceLayerURL}/b1s/v1/SBOBobService_SetCurrencyRate`, {
+        "Currency": "usd",
+        "RateDate": sapDate,
+        "Rate": usdValue
+      },
+      {
+        headers: {
+          "Cookie": cookie
         }
-      );
-      if(SAPResponse.status === 204) {
-        console.log('Se actualizó correctamente tipo de cambio en SAP BUSINESS ONE');
       }
-      //Set currency EUR
-      SAPResponse = await axios.post(`https://${serviceLayerURL}/b1s/v1/SBOBobService_SetCurrencyRate`, {
-          "Currency": "eur",
-          "RateDate": sapDate,
-          "Rate": eurValue
-        },
-        {
-          headers: {
-            "Cookie": cookie
-          }
+    );
+    if (SAPResponse.status === 204) {
+      console.log('Se actualizó correctamente tipo de cambio en SAP BUSINESS ONE');
+    }
+    //Set currency EUR
+    SAPResponse = await axios.post(`https://${serviceLayerURL}/b1s/v1/SBOBobService_SetCurrencyRate`, {
+        "Currency": "eur",
+        "RateDate": sapDate,
+        "Rate": eurValue
+      },
+      {
+        headers: {
+          "Cookie": cookie
         }
-      );
-      if(SAPResponse.status === 204) {
-        console.log('Se actualizó correctamente tipo de cambio en SAP BUSINESS ONE');
       }
-      //Get currency USD
-      SAPResponse = await axios.post(`https://${serviceLayerURL}/b1s/v1/SBOBobService_GetCurrencyRate`, {
-          "Currency": "usd",
-          "Date": sapDate
-        },
-        {
-          headers: {
-            "Cookie": cookie
-          }
+    );
+    if (SAPResponse.status === 204) {
+      console.log('Se actualizó correctamente tipo de cambio en SAP BUSINESS ONE');
+    }
+    //Get currency USD
+    SAPResponse = await axios.post(`https://${serviceLayerURL}/b1s/v1/SBOBobService_GetCurrencyRate`, {
+        "Currency": "usd",
+        "Date": sapDate
+      },
+      {
+        headers: {
+          "Cookie": cookie
         }
-      );
-      console.log(`Valor recuperado USD del día ${today} desde de SAP BUSINESS ONE: ${SAPResponse.data}`);
-      //Get currency EUR
-      SAPResponse = await axios.post(`https://${serviceLayerURL}/b1s/v1/SBOBobService_GetCurrencyRate`, {
-          "Currency": "eur",
-          "Date": sapDate
-        },
-        {
-          headers: {
-            "Cookie": cookie
-          }
+      }
+    );
+    console.log(`Valor recuperado USD del día ${today} desde de SAP BUSINESS ONE: ${SAPResponse.data}`);
+    //Get currency EUR
+    SAPResponse = await axios.post(`https://${serviceLayerURL}/b1s/v1/SBOBobService_GetCurrencyRate`, {
+        "Currency": "eur",
+        "Date": sapDate
+      },
+      {
+        headers: {
+          "Cookie": cookie
         }
-      );
-      console.log(`Valor EUR recuperado del día ${today} desde de SAP BUSINESS ONE: ${SAPResponse.data}`);
-      await axios.post(`https://${serviceLayerURL}/b1s/v1/Logout`);
-      return {
-        success: true,
-        usd: usdValue,
-        eur: eurValue,
-        date: today
-      };
+      }
+    );
+    console.log(`Valor EUR recuperado del día ${today} desde de SAP BUSINESS ONE: ${SAPResponse.data}`);
+    await axios.post(`https://${serviceLayerURL}/b1s/v1/Logout`);
+    return {
+      success: true,
+      usd: usdValue,
+      eur: eurValue,
+      date: today
+    };
   } catch (e) {
     console.error('ERROR:');
     console.error(e.response.data);
